6e065d827c2694fa8552bbeca3c0a7f2970309ac
Rapphy243 <36800640+rapphy243@users.noreply.github.com>
2025-03-20 10:03:56 -0500

    Set up game over routine

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -161,6 +161,18 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         livesLabel.text = "Lives: \(lives)"
     }
     
+    func gameOver(winner: Bool) {
+        playingGame = false
+        playLabel.alpha = 1
+        resetGame()
+        if winner {
+            playLabel.text = "You win! Tap to play again"
+        }
+        else {
+            playLabel.text = "You lose! Tap to play again"
+        }
+    }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
             let location = touch.location(in: self)
@@ -194,12 +206,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     func didBegin(_ contact: SKPhysicsContact) {
         if contact.bodyA.node?.name == "brick" || contact.bodyB.node?.name == "brick" {
             print("You win!")
-            brick.removeFromParent()
-            ball.removeFromParent()
+            gameOver(winner: true)
         }
         if contact.bodyA.node?.name == "loseZone" || contact.bodyB.node?.name == "loseZone" {
             print("You lose!")
-            ball.removeFromParent()
+            gameOver(winner: false)
         }
     }
 
