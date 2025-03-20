8652dc20b6239c2eb39a79e922744ad924a361c6
Rapphy243 <36800640+rapphy243@users.noreply.github.com>
2025-03-20 10:00:41 -0500

    Setup display labels

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -15,6 +15,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     private var brick = SKSpriteNode()
     private var loseZone = SKSpriteNode()
     
+    private var playLabel = SKLabelNode()
+    private var livesLabel = SKLabelNode()
+    private var scoreLabel = SKLabelNode()
+    private var playingGame = false
+    private var score = 0
+    private var lives = 3
+    
     override func didMove(to view: SKView) {
         // this stuff happens once (when the app opens)
         physicsWorld.contactDelegate = self
@@ -22,11 +29,12 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         createBackground()
         resetGame()
         makeLoseZone()
-        kickBall()
+        makeLabels()
     }
     
     func resetGame() {
         // this stuff happens before each game starts
+        updateLabels()
         makeBall()
         makePaddle()
         makeBrick()
@@ -42,10 +50,21 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         for i in 0...1 {
             let starsBackground = SKSpriteNode(texture: stars)
             starsBackground.zPosition = -1
-            starsBackground.position = CGPoint(x: 0, y: starsBackground.size.height * CGFloat(i))
+            starsBackground.position = CGPoint(
+                x: 0,
+                y: starsBackground.size.height * CGFloat(i)
+            )
             addChild(starsBackground)
-            let moveDown = SKAction.moveBy(x: 0, y: -starsBackground.size.height, duration: 20)
-            let moveReset = SKAction.moveBy(x: 0, y: starsBackground.size.height, duration: 0)
+            let moveDown = SKAction.moveBy(
+                x: 0,
+                y: -starsBackground.size.height,
+                duration: 20
+            )
+            let moveReset = SKAction.moveBy(
+                x: 0,
+                y: starsBackground.size.height,
+                duration: 0
+            )
             let moveLoop = SKAction.sequence([moveDown, moveReset])
             let moveForever = SKAction.repeatForever(moveLoop)
             starsBackground.run(moveForever)
@@ -74,14 +93,19 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         ball.physicsBody?.restitution = 1
         //does not slow down over time
         ball.physicsBody?.linearDamping = 0
-        ball.physicsBody?.contactTestBitMask = (ball.physicsBody?.collisionBitMask)!
+        ball.physicsBody?.contactTestBitMask = (
+            ball.physicsBody?.collisionBitMask
+        )!
         
         addChild(ball)
     }
     
     func makePaddle() {
         paddle.removeFromParent()
-        paddle = SKSpriteNode(color: .white, size: CGSize(width: frame.width / 4, height: 20))
+        paddle = SKSpriteNode(
+            color: .white,
+            size: CGSize(width: frame.width / 4, height: 20)
+        )
         paddle.name = "paddle"
         paddle.physicsBody = SKPhysicsBody(rectangleOf: paddle.size)
         paddle.physicsBody?.isDynamic = false
@@ -99,7 +123,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     func makeLoseZone() {
-        loseZone = SKSpriteNode(color: .red, size: CGSize(width: frame.width, height: 50))
+        loseZone = SKSpriteNode(
+            color: .red,
+            size: CGSize(width: frame.width, height: 50)
+        )
         loseZone.position = CGPoint(x: frame.midX, y: frame.minY + 25)
         loseZone.name = "loseZone"
         loseZone.physicsBody = SKPhysicsBody(rectangleOf: loseZone.size)
@@ -107,17 +134,60 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         addChild(loseZone)
     }
     
+    func makeLabels() {
+        playLabel.fontSize = 24
+        playLabel.text = "Tap to start"
+        playLabel.fontName = "Ariel"
+        playLabel.position = CGPoint(x: frame.midX, y: frame.midY - 50)
+        playLabel.name = "playLabel"
+        addChild(playLabel)
+        
+        livesLabel.fontSize = 18
+        livesLabel.fontColor = .black
+        livesLabel.fontName = "Ariel"
+        livesLabel.position = CGPoint(x: frame.minX + 50, y: frame.minY + 18)
+        addChild(livesLabel)
+        
+        scoreLabel.fontSize = 18
+        scoreLabel.fontColor = .black
+        scoreLabel.fontName = "Ariel"
+        scoreLabel.position = CGPoint(x: frame.minX - 50, y: frame.minY + 18)
+        addChild(scoreLabel)
+        
+    }
+    
+    func updateLabels() {
+        scoreLabel.text = "Score: \(score)"
+        livesLabel.text = "Lives: \(lives)"
+    }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
             let location = touch.location(in: self)
-            paddle.position.x = location.x
+            if playingGame {
+                paddle.position.x = location.x
+            }
+            else {
+                for node in nodes(at: location) {
+                    if node.name == "playLabel" {
+                        playingGame = true
+                        node.alpha = 0
+                        score = 0
+                        lives = 3
+                        updateLabels()
+                        kickBall()
+                    }
+                }
+            }
         }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
             let location = touch.location(in: self)
-            paddle.position.x = location.x
+            if playingGame {
+                paddle.position.x = location.x
+            }
         }
     }
     
